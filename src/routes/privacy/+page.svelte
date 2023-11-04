e29c9b6f5e7526b4a94f70e5e7c358126411ffb0
bnzodoumkouo <beaudelaire.nzodoumkouo@straumann.com>
2023-11-04 13:38:33 -0400

    updated portfolio url

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,7 +1,7 @@
 <div class="prose">
 
     <div class="mb-10">
-        <h2>What data does flaming.codes collect?</h2>
+        <h2>What data does portfolio-beaudelaire.web.app collect?</h2>
         <p>
             portfolio-beaudelaire.web.app doesn't collect any personal information about you. No cookies at all get created, not by
             the beaudelaire.github.io itself or any other 3rd party company.
